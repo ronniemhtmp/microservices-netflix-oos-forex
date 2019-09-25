58c8171dc983edd9e23227e05d877d5d7bafba80
Ronnie Moncayo <rmoncayo@emagic.fin.ec>
2019-09-25 15:35:19 -0500

    finish

--- currency-conversion-service/src/main/java/dev/ronniemoncayo/currencyconversionservice/CurrencyConversionServiceApplication.java
+++ currency-conversion-service/src/main/java/dev/ronniemoncayo/currencyconversionservice/CurrencyConversionServiceApplication.java
@@ -11,6 +11,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 public class CurrencyConversionServiceApplication {
 
 	public static void main(String[] args) {
+		
 		SpringApplication.run(CurrencyConversionServiceApplication.class, args);
 	}
 
